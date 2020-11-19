e2fe403068662c85bcf24cf919c3574382dbe0ed
audrey2806 <72178283+audrey2806@users.noreply.github.com>
2020-11-19 10:57:16 +0800

    Add files via upload

---
+++ hadiah teori/polimorfisme/volumee.java
@@ -0,0 +1,20 @@
+public class volumee{
+	
+	private double jari2;
+	final double PI = 3.14;
+	
+
+	
+	public volumee(double jari2){
+		this.jari2 = jari2;
+	}
+	protected void hasil(){
+		
+	}
+	public double getjari(){
+		return jari2;
+	}
+	
+	
+	
+}
